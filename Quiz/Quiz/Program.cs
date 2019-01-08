2ebb110d76855852295be6bfcde86b3837c0bd31
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-08 09:37:19 -0800

    first question

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old am I?");
+            Console.Read();
+            int answer = Convert.ToInt32(Console.Read());
+            
+                if (answer == 34)
+            {
+                Console.WriteLine("Correct!");
+            }
+            
+            else 
+            { 
+                Console.WriteLine("Not even close!");
+            }
+           
+
+
+        }
+        //Console.WriteLine("what is my favorite color");
+    }
+}
